2fa74e15b998a0f4683062721700df5fa05cd02c
Fatih <brilianfatihwicaksono2005@mail.ugm.ac.id>
2026-09-26 13:46:09 +0700

    fix: resolve import path in router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
-import Tasks from './views/Tasks.vue'
+import Home from '../views/Home.vue'
+import Tasks from '../views/Tasks.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
